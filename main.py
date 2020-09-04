1a332ae83541977a8e22ce156a8050aab8e0f13b
Huong Nam Vu <namvuhuong@gmail.com>
2020-09-04 17:14:17 +0700

    change to list

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ import time
 
 
 def compute_max_profit(lst):
-    return max(lst)
+    return max(lst)[1]
+
 
 def parse_text(url, file):
     with open(file, 'w') as f:
         f.write('date,rate\n')
-    driver = webdriver.Chrome(r'D:\courses\ds101\ds101ex02template\chromedriver_win32\chromedriver.exe') 
+    driver = webdriver.Chrome(
+        r'D:\courses\ds101\ds101ex02template\chromedriver_win32\chromedriver.exe')
     driver.get(url)
     start_date = date(2020, 6, 1)
     end_date = date(2020, 7, 31)
@@ -31,19 +33,21 @@ def parse_text(url, file):
             list_tds = row.find_elements_by_tag_name('td')
             if len(list_tds) >= 5:
                 if list_tds[1].text.strip() == 'AUD':
-                    rate = list_tds[4].text.replace(',','')
+                    rate = list_tds[4].text.replace(',', '')
                     with open('data/exchange_rate.aud.06_2020.07_2020.csv', 'a') as f:
                         f.write(",". join((date_string, str(rate)+"\n")))
         start_date += delta
 
+
 if __name__ == "__main__":
 
-    parse_text('https://portal.vietcombank.com.vn/Personal/TG/Pages/ty-gia.aspx', 'data/exchange_rate.aud.06_2020.07_2020.csv')
+    parse_text('https://portal.vietcombank.com.vn/Personal/TG/Pages/ty-gia.aspx',
+               'data/exchange_rate.aud.06_2020.07_2020.csv')
     # load your data frame from data/exchange_rate.aud.06_2020.07_2020.csv
     df = pd.read_csv('data/exchange_rate.aud.06_2020.07_2020.csv')
 
     # convert to list
-    list_exchange_rates = df['rate'].tolist()
+    list_exchange_rates = df.values.tolist()
 
     # compute max profit we can achieve
     max_profit = compute_max_profit(list_exchange_rates)
